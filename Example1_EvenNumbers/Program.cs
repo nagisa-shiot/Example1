242895ca08350986c65a587b71757799d72ce8c6
Shchiptsov Danil <bondar-seryy@mail.ru>
2022-08-10 18:38:34 +0800

    EvenNumbers

---
+++ Example1_EvenNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿int i = 1;
+bool not = true;
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Чётные числа от 1 до " + num);
+while (i <= num)
+{
+        if (i % 2 != 1)
+        {
+                Console.Write(i + ", ");
+                not = false;
+        }
+        i++;
+}
+
+if (not)
+{
+        Console.WriteLine("Нет чётных чисел!");
+}
+
