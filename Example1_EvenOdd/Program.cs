3064b07e7301c39b397ccf1e945fa15fa93b7fb7
Shchiptsov Danil <bondar-seryy@mail.ru>
2022-08-10 22:48:54 +0800

    исправил неточности

--- Example1_EvenOdd/Program.cs
+++ Example1_EvenOdd/Program.cs
@@ -1,11 +1,11 @@
 ﻿Console.WriteLine("Введите число:");
 int num = Convert.ToInt32(Console.ReadLine());
 
-if (num % 2 == 1)
+if (num % 2 == 0)
 {
-        Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
+        Console.WriteLine("Число " + num + " является: ЧЁТНЫМ");
 }
 else
 {
-        Console.WriteLine("Число " + num + "является: ЧЁТНЫМ");
+        Console.WriteLine("Число " + num + " является: НЕЧЁТНЫМ");
 }
