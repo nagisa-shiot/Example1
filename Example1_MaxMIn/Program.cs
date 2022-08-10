d21cb8ffd09dd78d7e251f7a8a02f0913b6cb81a
Shchiptsov Danil <bondar-seryy@mail.ru>
2022-08-10 17:45:15 +0800

    Project Max and Min

---
+++ Example1_MaxMIn/Program.cs
@@ -0,0 +1,15 @@
+﻿int a=5;
+int b=7;
+
+int max=a;
+int min=b;
+
+if (a>max) max=a;
+if (b>max) max=b;
+if (a<min) min=a;
+if (b<min) min=b;
+
+Console.Write("max=");
+Console.WriteLine(max);
+Console.Write("min=");
+Console.WriteLine(min);
